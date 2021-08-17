2b92947aaa9dd19cf4be87233c41fce6c9948839
abgr <tbpmtbpm@gmail.com>
2021-08-17 23:45:50 +0430

    impr

--- pointer.go
+++ pointer.go
@@ -6,29 +6,23 @@ import (
 )
 
 type Ptr struct {
-	ptr unsafe.Pointer
+	Ptr unsafe.Pointer
 }
 
-func AsPtr(ptr unsafe.Pointer) Ptr {
-	return Ptr{ptr}
-}
 func (ptr Ptr) Set(another unsafe.Pointer) {
-	atomic.StorePointer(&ptr.ptr, another)
-}
-func (ptr Ptr) Ptr() unsafe.Pointer {
-	return ptr.ptr
+	atomic.StorePointer(&ptr.Ptr, another)
 }
 func (ptr Ptr) Get() (another unsafe.Pointer) {
-	return atomic.LoadPointer(&ptr.ptr)
+	return atomic.LoadPointer(&ptr.Ptr)
 }
 func (ptr Ptr) Swap(new unsafe.Pointer) (old unsafe.Pointer) {
-	return atomic.SwapPointer(&ptr.ptr, new)
+	return atomic.SwapPointer(&ptr.Ptr, new)
 }
 func (ptr Ptr) CAS(test, new unsafe.Pointer) bool {
-	return atomic.CompareAndSwapPointer(&ptr.ptr, test, new)
+	return atomic.CompareAndSwapPointer(&ptr.Ptr, test, new)
 }
 func (ptr Ptr) SwapValue(value interface{}) (old interface{}) {
-	return *(*interface{})(atomic.SwapPointer(&ptr.ptr, unsafe.Pointer(&value)))
+	return *(*interface{})(atomic.SwapPointer(&ptr.Ptr, unsafe.Pointer(&value)))
 }
 func (ptr Ptr) SetValue(value interface{}) {
 	ptr.Set(unsafe.Pointer(&value))
